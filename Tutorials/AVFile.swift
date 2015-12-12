89ace782145ac0f64eb4d69cee19f1bb0cf19cc2
Ryan Lovelett <ryan@lovelett.me>
2015-12-12 12:56:32 -0500

    Begin tutorial

---
+++ Tutorials/AVFile.swift
@@ -0,0 +1,21 @@
+//
+//  AVFile.swift
+//  Tutorials
+//
+//  Created by Ryan Lovelett on 12/12/15.
+//  Copyright © 2015 Tokuda Shusuke. All rights reserved.
+//
+
+import Foundation
+
+class AVFile {
+    init(path: String) {
+
+    }
+}
+
+extension AVFile: CustomStringConvertible {
+    var description: String {
+        return "Not implemented yet."
+    }
+}
--- Tutorials/main.swift
+++ Tutorials/main.swift
@@ -8,6 +8,17 @@
 
 import Foundation
 
-print("Hello, World!")
+guard let filePath = Process.arguments.reverse().first else {
+    print("No path to a video file was provided.")
+    exit(1)
+}
 
-Tutorial1().main("/Users/ryan/Desktop/test.ts")
+let file = AVFile(path: filePath)
+
+/**
+Input #0, mpegts, from '/Users/ryan/Desktop/test.ts':
+  Duration: 00:00:22.25, start: 58812.807589, bitrate: 7774 kb/s
+    Stream #0:0[0x31]: Video: mpeg2video (Main), yuv420p(tv), 1920x1080 [SAR 1:1 DAR 16:9], Closed Captions, max. 19392 kb/s, 29.97 fps, 29.97 tbr, 90k tbn, 59.94 tbc
+    Stream #0:1[0x34]: Audio: ac3, 48000 Hz, 5.1(side), fltp, 384 kb/s
+*/
+print(file)
